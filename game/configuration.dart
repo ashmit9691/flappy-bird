7d47388ad3afe4d591e0a3f0b619208caeee314a
Ashmit Srivastava <144439630+ashmit9691@users.noreply.github.com>
2024-10-04 22:08:17 +0530

    Add files via upload

---
+++ game/configuration.dart
@@ -0,0 +1,8 @@
+class Config {
+  static const groundHeight = 110.0;
+  static const gameSpeed = 200.0;
+  static const pipeInterval = 1.5;
+  static const birdVelocity = 100;
+  static const gravity = -300.0;
+  static const cloudsHeight = 70.0;
+}
